9ec58830abe1c326dc0e6e5f51e6748370f2115d
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-06 15:41:52 -0800

    Create shell of locations container

---
+++ src/LocationsContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class LocationsContainer extends Component {
+  render() {
+    return (
+      <div>Locations Container</div>
+    )
+  }
+}
+
+export default LocationsContainer
