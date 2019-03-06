454c104406da4e230f8a5cbe16b4e16637ad08be
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-05 21:17:51 -0800

    Add app bar demo.

--- src/HomeContainer/index.js
+++ src/HomeContainer/index.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Typography, CssBaseline, AppBar, Drawer, withStyles, Toolbar } from '@material-ui/core';
+
+const drawerWidth = 240
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3
+  },
+  toolbar: theme.mixins.toolbar
+})
 
 class HomeContainer extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  
   render() {
+    const { classes } = this.props
     return (
-      <div>Home Page</div>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.appBar}>
+              Schedulest
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer>
+
+        </Drawer>
+      </div>
     )
   }
 }
 
-export default HomeContainer;
+HomeContainer.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(HomeContainer);
