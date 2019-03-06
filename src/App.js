16c661b249451ab5527eb0e3f90af83ec187e146
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-05 20:54:25 -0800

    install react router, set up home and login routes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
+import { Route, Redirect } from 'react-router-dom';
 import './App.css';
 import HomeContainer from './HomeContainer';
 import LoginContainer from './LoginContainer';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      logged: true
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Typography variant="h2">Hello World</Typography>
-        <HomeContainer />
-        <LoginContainer />
+        <Typography variant="h2">Schedulest</Typography>
+        <Route exact path="/" render={ props =>
+          this.state.logged ?
+          <HomeContainer {...props} /> :
+          <Redirect to={{
+            pathname: "/auth"
+          }} />
+          } />
+        <Route exact path="/auth" component={LoginContainer} />
       </div>
     );
   }
