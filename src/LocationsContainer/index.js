4464fec1517beca6aeba7e73c3147cb3afb3e3b5
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-08 13:15:57 -0800

    Link to location details page from locations list.

--- src/LocationsContainer/index.js
+++ src/LocationsContainer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 import { Typography, Paper, List, ListItem, Button, ListItemSecondaryAction, ListItemText, Fab } from '@material-ui/core';
 import { DateRange as DateRangeIcon, Add as AddIcon } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
@@ -21,6 +22,10 @@ const styles = theme => ({
   headerDiv:  {
     position: 'relative',
     marginBottom: theme.spacing.unit * 3
+  },
+  navLink: {
+    textDecoration: 'none',
+    color: 'inherit'
   }
 })
 
@@ -47,17 +52,19 @@ class LocationsContainer extends Component {
     const locationLis = locations.map(({name, description}, idx) => {
       return (
         <ListItem key={idx}>
-        <ListItemText
-          primary={name}
-          secondary={description}
-          />
-        <ListItemSecondaryAction>
-          <Button className={classes.button} size="small" variant="contained" aria-label="Location Schedule">
-          <DateRangeIcon />
-            Schedule
-          </Button>
-        </ListItemSecondaryAction>
-      </ListItem>
+          <ListItemText
+            primary={name}
+            secondary={description}
+            />
+          <ListItemSecondaryAction>
+            <RouterLink to={`/locations/${idx}`} className={classes.navLink}>
+              <Button className={classes.button} size="small" variant="contained" aria-label="Location Schedule">
+              <DateRangeIcon />
+                Schedule
+              </Button>
+            </RouterLink>
+          </ListItemSecondaryAction>
+        </ListItem>
       )
     })
     
