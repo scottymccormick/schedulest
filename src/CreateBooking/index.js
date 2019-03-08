52b18704d79d23ec4b7f01a1b2937469f0acb029
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-07 16:31:43 -0800

    Add location to inputs. Track values in state.

--- src/CreateBooking/index.js
+++ src/CreateBooking/index.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, FormControl, Select, MenuItem, InputLabel } from '@material-ui/core'
 
 class BookingDialog extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      owner: 'John User',
+      title: '',
+      location: ''
+    }
+  }
+  handleChange = e => {
+    console.log(e)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
   render() {
+    console.log(this.state)
     return (
       <Dialog open={this.props.open} onClose={this.props.onClose}>
         <DialogTitle>Create Booking</DialogTitle>
@@ -12,28 +28,28 @@ class BookingDialog extends Component {
           </DialogContentText>
           <form action="">
             <FormControl fullWidth>
-              <TextField margin="dense" label="Name"name="owner" type="text"
-                value="John User" required  />
+              <TextField margin="dense" label="Name" name="owner" type="text"
+                value={this.state.owner} onChange={this.handleChange} required  />
             </FormControl>
             <FormControl fullWidth>
               <TextField margin="dense" label="Title (optional)" name="title"
-                type="text"  />
+                type="text" onChange={this.handleChange} />
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>Location</InputLabel>
               <Select
-                // value={this.state.age}
-                // onChange={this.handleChange}
-                // inputProps={{
-                //   name: 'location'
-                // }}
+                value={this.state.location}
+                onChange={this.handleChange}
+                inputProps={{
+                  name: 'location'
+                }}
                 >
-                <MenuItem>
+                <MenuItem value={0}>
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={1}>Studio 1</MenuItem>
+                <MenuItem value={2}>Studio 2</MenuItem>
+                <MenuItem value={3}>Studio 3</MenuItem>
               </Select>
             </FormControl>
             
