a9a876f4212fce59d3f43bf7026370d272f84d18
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-08 13:34:01 -0800

    Add back arrow for location detail page.

--- src/LocationDetail/index.js
+++ src/LocationDetail/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
-import { Typography, Paper } from '@material-ui/core'
+import { Typography, Paper, Button } from '@material-ui/core'
+import { ArrowBackIos } from '@material-ui/icons'
 
 const styles = theme => ({
   paperArea: {
@@ -9,6 +11,12 @@ const styles = theme => ({
   headerDiv:  {
     position: 'relative',
     marginBottom: theme.spacing.unit * 3
+  },
+  headerButton: {
+    top: 0,
+    left: 0,
+    position: 'absolute',
+    zIndex: 10
   }
 });
 
@@ -18,6 +26,12 @@ const LocationDetail = (props) => {
   return (
     <main>
       <div className={classes.headerDiv}>
+        <RouterLink to="/locations">
+          <Button variant="contained" color="default" className={classes.headerButton}>
+            <ArrowBackIos size="small" />
+            Locations
+          </Button>
+        </RouterLink>
         <Typography variant="h4" gutterBottom component="h2" className={classes.headerDiv}>
           Studio X
         </Typography>
