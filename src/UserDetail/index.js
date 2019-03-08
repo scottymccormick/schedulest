8618189f746b2d0838f2ab2ddb05ede04cd951a9
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-08 14:20:08 -0800

    Add back button to user show page.

--- src/UserDetail/index.js
+++ src/UserDetail/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
+import { Typography, Button } from '@material-ui/core'
+import { ArrowBackIos } from '@material-ui/icons'
+import { withStyles } from '@material-ui/core/styles'
 
-const UserDetail = (props) => {
-  console.log(props.match.params.id)
+const styles = theme => ({
+  paperArea: {
+    padding: theme.spacing.unit * 2
+  },
+  headerDiv:  {
+    position: 'relative',
+    marginBottom: theme.spacing.unit * 3
+  },
+  headerButton: {
+    top: 0,
+    left: 0,
+    position: 'absolute',
+    zIndex: 10
+  }
+});
+
+const UserDetail = ({match, classes}) => {
+  console.log(match.params.id)
   return (
-    <Typography>
-      User Details
-    </Typography>
+    <main>
+      <div className={classes.headerDiv}>
+        <RouterLink to="/users">
+          <Button variant="contained" color="default" className={classes.headerButton}>
+            <ArrowBackIos size="small" />
+            Users
+          </Button>
+        </RouterLink>
+        <Typography variant="h4" gutterBottom component="h2" className={classes.headerDiv}>
+          User X
+        </Typography>
+      </div>
+    </main>
   )
 }
 
-export default UserDetail
+export default withStyles(styles)(UserDetail)
